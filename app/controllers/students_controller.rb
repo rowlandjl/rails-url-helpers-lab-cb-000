c755a180ed889f228d6fe78e704d71db148b33f6
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-31 02:36:22 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,7 +10,8 @@ class StudentsController < ApplicationController
   
   def activate
     @student = Student.find(params[:id])
-    
+    @student.active = !@student.active 
+    @student.save 
   end 
 
   private
