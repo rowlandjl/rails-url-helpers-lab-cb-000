5963dbeebbd0ef361cc597fcc3b7f12b07f83e41
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-31 02:21:37 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   resources :students, to: [:index, :show]
-  resources :students, path_names: 
+  resources :students, path_names: { edit: 'activate' }
 end
